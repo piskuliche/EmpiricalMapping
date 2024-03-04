ed2b0b0192078058c526dfc86a24cce026aa3bf0
Zeke Piskulich <piskulichz@gmail.com>
2024-03-04 10:56:12 -0600

    Update memory allocation in emp_setup.py

--- empmap/emp_setup.py
+++ empmap/emp_setup.py
@@ -117,7 +117,7 @@ class MapSetup:
         finp = open(calc_subdir+file_prefix+"%s.gjf" % frame_number, "w")
         fxyz = open(calc_subdir+file_prefix+"%s.xyz" % frame_number, "w")
         finp.write("%"+"NProcShared=%d\n" % self.nproc)
-        finp.write("%"+"Mem=%dB\n" % self.mem)
+        finp.write("%"+"Mem=%dGB\n" % self.mem)
         finp.write("%chk=calc.chk\n")
         rOHs = np.zeros(self.ngrid)
         for n in range(self.ngrid):
