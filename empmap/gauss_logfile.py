2961d07415838d9ea095f213a4be4f5335a43b5b
Zeke Piskulich <piskulichz@gmail.com>
2024-03-09 13:46:08 -0500

    updated gauss_logfile docs

--- empmap/gauss_logfile.py
+++ empmap/gauss_logfile.py
@@ -1,13 +1,63 @@
+"""
+This module contains a class to read Gaussian log files and extract data from them.
+
+Notes:
+------
+This class is used to read Gaussian log files and extract data from them. It is used to extract the energies and the dipole moments from the log files.
+
+Examples:
+---------
+>>> from empmap.gauss_logfile import GaussianLogFile
+>>> logfile = GaussianLogFile("file.log")
+>>> print(logfile.energies)
+>>> print(logfile.dipole)
+"""
+__all__ = ["GaussianLogFile"]
+
+
 import numpy as np
 
 
 class GaussianLogFile:
     def __init__(self, logname):
+        """ Initializes the GaussianLogFile class
+
+        Notes:
+        ------
+        This class is used to read Gaussian log files and extract data from them. It is used to extract the energies and the dipole moments from the log files.
+
+        Parameters:
+        ----------
+        logname : str
+            The name of the log file to read.
+
+        Returns:
+        -------
+        None
+
+        """
         self.logname = logname
         self.energies = self.grab_energies()
         self.dipole = self.grab_dipole()
 
-    def grab_energies(self):
+    def grab_energies(self, expected=None):
+        """ Grabs the energies from the log file
+
+        Parameters:
+        ----------
+        None
+
+        Returns:
+        -------
+        energies : array_like
+            The energies from the log file.
+
+        Raises:
+        ------
+        ValueError: 
+            If no energies are found in the log file.
+
+        """
         energies = []
         with open(self.logname, 'r') as f:
             for line in f:
@@ -18,9 +68,32 @@ class GaussianLogFile:
         if len(energies) == 0:
             raise ValueError("No energies found in the log file")
 
+        if expected is not None:
+            if len(energies) != expected:
+                raise ValueError("Expected %d energies, but found %d" %
+                                 (expected, len(energies)))
+
         return np.array(energies)
 
     def grab_dipole(self):
+        """ Grabs the dipole moments from the log file
+
+        Parameters:
+        ----------
+        None
+
+        Returns:
+        -------
+        dipole : array_like
+            The dipole moments from the log file.
+
+        Raises:
+        ------
+        ValueError: 
+            If no dipole moments are found in the log file.
+
+        """
+
         dipole = []
         prev_line = ""
         with open(self.logname, 'r') as f:
