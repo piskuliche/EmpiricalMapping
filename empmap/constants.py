37a01a7c24e32da061c5e76766ca11a163fdacea
Zeke Piskulich <piskulichz@gmail.com>
2024-03-09 13:45:36 -0500

    updated constants docstrings

--- empmap/constants.py
+++ empmap/constants.py
@@ -1,25 +1,83 @@
-# Constants classes
+""" This is a class for physical constants
+
+Notes
+-----
+
+This class is a simple way to store physical constants in a single place. It is
+intended to be used as a parent class for other classes, so that the constants
+can be accessed as attributes of the child classes.
+
+Examples
+--------
+
+>>> from empmap.constants import ConstantsManagement
+>>> constants = ConstantsManagement()
+>>> constants.PI
+
+"""
+
+__all__ = ["ConstantsManagement"]
+
 import numpy as np
 
 
 class MathConstants:
+    """ A class for mathematical constants
+
+    Notes
+    -----
+    This class is a simple way to store mathematical constants in a single place.
+    It is intended to be used as a parent class for other classes, so that the
+    constants can be accessed as attributes of the child classes.
+
+    """
     PI = np.pi
 
 
 class PhysicalConstants:
-    hbar_evs = 6.582119569e-16
+    """ A class for physical constants
+
+    Notes
+    -----
+    This class is a simple way to store physical constants in a single place.
+    It is intended to be used as a parent class for other classes, so that the
+    constants can be accessed as attributes of the child classes.
+
+    """
+
+    hbar_evs = 6.582119569e-16  # eV s
 
 
 class ConversionConstants:
-    evperau = 27.211386245988
-    angperau = 0.529177210903
-    aupergmol = 1822.992667
-    cmiperau = 2.1947463136320e5
-    evper_amuangpersqsec = 1.0364e-28
+    """ A class for conversion constants
+
+    Notes
+    -----
+    This class is a simple way to store conversion constants in a single place.
+    It is intended to be used as a parent class for other classes, so that the
+    constants can be accessed as attributes of the child classes.
+
+    """
+
+    evperau = 27.211386245988  # eV/au
+    angperau = 0.529177210903  # angstrom/au
+    aupergmol = 1822.992667  # amu/gram/mol
+    cmiperau = 2.1947463136320e5  # cm^-1/au
+    evper_amuangpersqsec = 1.0364e-28  # eV/(amu*ang/sec^2)
 
 
 # ConstantsManagement class
 class ConstantsManagement:
+    """ A class for managing constants
+
+    Notes
+    -----
+    This class is a wrapper class for the other constants classes. It is intended
+    to be used as a parent class for other classes, so that the constants can be
+    accessed as attributes of the child classes.
+
+    """
+
     def __init__(self):
         # Set constants from separate classes as attributes
         for cls in [MathConstants, PhysicalConstants, ConversionConstants]:
